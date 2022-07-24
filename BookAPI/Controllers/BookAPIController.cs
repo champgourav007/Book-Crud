116f322adaaab0967f5dd3439a063a57ddc32317
Champgourav007 <gourav.19b131001@abes.ac.in>
2022-07-24 18:43:03 +0530

    API proj is Added
    The MVC is converted to the API, all the work is done by the ASP.NET API and data is shown by the MVC

---
+++ BookAPI/Controllers/BookAPIController.cs
@@ -0,0 +1,54 @@
+﻿using BookAPI.Data;
+using BookAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookAPIController : ControllerBase
+    {
+        private readonly DataContext _db;
+
+        public BookAPIController(DataContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _db.Books.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<BookAPIDb>>> AddBook(BookAPIDb book)
+        {
+            try
+            {
+                await _db.Books.AddAsync(book);
+                await _db.SaveChangesAsync();
+                return Ok("Data is Added.");
+            }
+            catch
+            {
+                return BadRequest("Data is not Added.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            var book = await _db.Books.FindAsync(id);
+            if (book != null)
+            {
+                _db.Books.Remove(book);
+                await _db.SaveChangesAsync();
+                return Ok("Data is Deleted.");
+            }
+            return BadRequest("Data not Found.");
+        }
+    }
+}
--- BookCRUD/Controllers/BookController.cs
+++ BookCRUD/Controllers/BookController.cs
@@ -1,40 +1,63 @@
-﻿using BookCRUD.Data;
+﻿using BookAPI.Data;
+using BookAPI.Models;
 using BookCRUD.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Net.Http;
 
 namespace BookCRUD.Controllers
 {
     public class BookController : Controller
     {
-        private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public BookController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        public BookController(IWebHostEnvironment webHostEnvironment)
         {
-            _db = db;
             _webHostEnvironment = webHostEnvironment;
         }
 
+        HttpClient client = new HttpClient();
 
         // GET: Book
         public IActionResult Index()
         {
-            IEnumerable<BookViewModel> Books = _db.Books;
-            return View(Books);
+            List<BookAPIDb> books = new List<BookAPIDb>();
+            client.BaseAddress = new Uri("https://localhost:7152/api/BookAPI");
+            var response = client.GetAsync("BookAPI");
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<BookAPIDb>>();
+                display.Wait();
+                books = display.Result;
+            }
+
+            return View(books);
         }
 
-        // GET: Book/Details/5
+        //GET: Book/Details/5
         public IActionResult Details(int id)
         {
-            var book = _db.Books.Find(id);
-            if(book == null)
+            //BookAPIDb book = new BookAPIDb();
+            client.BaseAddress = new Uri(String.Format("https://localhost:7152/api/BookAPI/{0}", id));
+            var response = client.GetAsync("BookAPI");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<BookAPIDb>();
+                display.Wait();
+                var book = display.Result;
+                return View(book);
+            }
+            else
             {
                 return NotFound();
             }
-            return View(book);
+            
         }
 
         // GET: Book/Create
@@ -47,23 +70,31 @@ namespace BookCRUD.Controllers
         // POST: Book/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(BookViewModel obj, IFormFile? file)
+        public IActionResult Create(BookAPIDb obj, IFormFile? file)
         {
-            if(file != null)
+            if (file != null)
             {
-                obj.Image = SaveImagePath(obj, file);
+                obj.Image = SaveImagePath(file);
             }
-            if(ModelState.IsValid)
+
+
+            client.BaseAddress = new Uri("https://localhost:7152/api/BookAPI");
+            var response = client.PostAsJsonAsync<BookAPIDb>("BookAPI", obj);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
             {
-                _db.Books.Add(obj);
-                _db.SaveChanges();
-                TempData["success"] = "Book is Added Successfully!";
+                TempData["success"] = "Data is Successfully Added";
                 return RedirectToAction("Index");
+                
             }
+
+            TempData["error"] = "Some Error Occured Please Re-Enter the Data";
             return View();
         }
 
-        private string SaveImagePath(BookViewModel obj, IFormFile file)
+        private string SaveImagePath(IFormFile file)
         {
             string wwwRootPath = _webHostEnvironment.WebRootPath;
             string fileName = Guid.NewGuid().ToString();
@@ -76,72 +107,72 @@ namespace BookCRUD.Controllers
 
             return @"\Images\" + fileName + extension;
         }
-        
 
-        // GET: Book/Edit/5
+
+        //// GET: Book/Edit/5
         public IActionResult Edit(int id)
         {
-            var book = _db.Books.Find(id);
-            if(book == null)
-            {
-                TempData["error"] = "Data Not Found!";
-                return View("Index");
-            }
-            return View(book);
+            //var book = _db.Books.Find(id);
+            //if (book == null)
+            //{
+            //    TempData["error"] = "Data Not Found!";
+            //    return View("Index");
+            //}
+            //return View(book);
         }
 
         // POST: Book/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(BookViewModel obj, IFormFile? file)
-        {
-            var path = "0";
-            if (file != null)
-            {
-                path = SaveImagePath(obj, file);
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit(BookViewModel obj, IFormFile? file)
+        //{
+        //    var path = "0";
+        //    if (file != null)
+        //    {
+        //        path = SaveImagePath(obj, file);
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                _db.Books.Update(obj);
-                var book = _db.Books.Find(obj.Id);
-                book.Image = path;
-                _db.SaveChanges();
-                TempData["success"] = "Data is Edited Successfully!";
-                return RedirectToAction("Details",obj);
-            }
-            return View();
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Books.Update(obj);
+        //        var book = _db.Books.Find(obj.Id);
+        //        book.Image = path;
+        //        _db.SaveChanges();
+        //        TempData["success"] = "Data is Edited Successfully!";
+        //        return RedirectToAction("Details",obj);
+        //    }
+        //    return View();
+        //}
 
         // GET: Book/Delete/5
-        public IActionResult Delete(int id)
-        {
-            if(id == null || id == 0)
-            {
-                TempData["error"] = "Data not Found!";
-                return View("Index");
-            }
-            var book = _db.Books.Find(id);
-            
-            return View(book);
-        }
+        //public IActionResult Delete(int id)
+        //{
+        //    if(id == null || id == 0)
+        //    {
+        //        TempData["error"] = "Data not Found!";
+        //        return View("Index");
+        //    }
+        //    var book = _db.Books.Find(id);
+
+        //    return View(book);
+        //}
 
         // POST: Book/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(BookViewModel obj)
-        {
-            try
-            {
-                _db.Books.Remove(obj);
-                _db.SaveChanges();
-                TempData["success"] = "Data is deleted successfully!";
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Delete(BookViewModel obj)
+        //{
+        //    try
+        //    {
+        //        _db.Books.Remove(obj);
+        //        _db.SaveChanges();
+        //        TempData["success"] = "Data is deleted successfully!";
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
